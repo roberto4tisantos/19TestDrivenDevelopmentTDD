33648608e7fbad6fa3c429ecfcd3922ac33d46ac
FORTIS Roberto <roberto.4ti@gmail.com>
2025-02-10 22:19:15 -0500

    update

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,11 +1,30 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
+import viteConfig from './vite.config';
+
+// export default defineConfig({
+//   component: {
+//     devServer: {
+//       framework: "react",
+//       bundler: "vite",
+//     },
+//     specPattern: "cypress/component/**/*.cy.{ts,tsx}",
+//   },
+// });
 
 export default defineConfig({
   component: {
+    port: 5173,
     devServer: {
-      framework: "react",
-      bundler: "vite",
+      framework: 'react',
+      bundler: 'vite',
+      viteConfig,
     },
-    specPattern: "cypress/component/**/*.cy.{ts,tsx}",
+  },
+
+  e2e: {
+    baseUrl: 'http://localhost:3001',
+    setupNodeEvents(_on, _config) {
+      // implement node event listeners here
+    },
   },
 });
